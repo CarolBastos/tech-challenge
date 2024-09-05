3f25ae2a43fa5b2ec21816770b21d98f434ace0d
carolbastos <carol@wwcalc.com>
2024-09-05 14:03:31 -0400

    Adicionando arquivos de exemplo

---
+++ pages/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../components/button/button";
+import "../styles/globals.scss"
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Teste titulo</h1>
+      <a>Link de teste</a>
+      <Button>Clique aqui</Button>
+    </div>
+  );
+};
+
+export default HomePage;
