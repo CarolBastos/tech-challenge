77789953864018abe84e6f0a1d911d3432ddcb86
carolbastos <carol@wwcalc.com>
2024-09-14 13:29:12 -0400

    Ajustando estilos do saldo

--- app/components/balance/balance.tsx
+++ app/components/balance/balance.tsx
@@ -10,7 +10,7 @@ export default function Balance() {
   };
 
   return (
-    <div className="balance rounded-lg p-6 grid grid-cols-2 gap-4">
+    <div className="balance rounded-lg p-6 grid grid-cols-2 gap-4 ">
       <div className="col-span-1">
         <h1 className="balance__title text-white text-[25px] font-semibold">
           Olá, Joana! :)
@@ -19,14 +19,14 @@ export default function Balance() {
           Quinta-feira, 08/09/2022
         </h2>
       </div>
-      <div className="col-span-1 ">
-        <div className="flex items-center space-x-2">
-          <span className="text-white">Saldo</span>
-          <EyeIcon className="w-6 h-6" />
+      <div className="col-span-1 balance__container-secondary content-center">
+        <div className="balance__box pb-4 border-b-2 w-[180px] flex items-center space-x-2">
+          <p className="text-white text-xl font-semibold pr-[25px]">Saldo</p>
+          <EyeIcon className="w-6 h-6 balance__icon-open" />
         </div>
 
-        <span className="text-white">Conta Corrente</span>
-        <p className="text-white">R$ 2.500,00</p>
+        <p className="text-white mt-4 pb-2 text-[16px] font-normal">Conta Corrente</p>
+        <p className="text-white text-[31px] font-normal">R$ 2.500,00</p>
       </div>
     </div>
   );
