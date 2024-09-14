6a61ac0f569314b26766a4ea7beba30a0bc8ef06
carolbastos <carol@wwcalc.com>
2024-09-14 13:35:52 -0400

    adicionando função de ocultar saldo

--- app/components/balance/balance.tsx
+++ app/components/balance/balance.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { EyeIcon } from "@heroicons/react/solid";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
 import "./balance.scss";
 
 export default function Balance() {
-  const [activeLink, setActiveLink] = useState("home");
+  const [isBalanceVisible, setIsBalanceVisible] = useState(true);
 
-  const handleClick = (link) => {
-    setActiveLink(link);
+  const toggleBalanceVisibility = () => {
+    setIsBalanceVisible(!isBalanceVisible);
   };
 
   return (
@@ -22,11 +22,27 @@ export default function Balance() {
       <div className="col-span-1 balance__container-secondary content-center">
         <div className="balance__box pb-4 border-b-2 w-[180px] flex items-center space-x-2">
           <p className="text-white text-xl font-semibold pr-[25px]">Saldo</p>
-          <EyeIcon className="w-6 h-6 balance__icon-open" />
+          {isBalanceVisible ? (
+            <EyeIcon
+              className="w-6 h-6 balance__icon-open cursor-pointer"
+              onClick={toggleBalanceVisibility}
+            />
+          ) : (
+            <EyeOffIcon
+              className="w-6 h-6 balance__icon-closed cursor-pointer"
+              onClick={toggleBalanceVisibility}
+            />
+          )}
         </div>
 
-        <p className="text-white mt-4 pb-2 text-[16px] font-normal">Conta Corrente</p>
-        <p className="text-white text-[31px] font-normal">R$ 2.500,00</p>
+        <p className="text-white mt-4 pb-2 text-[16px] font-normal">
+          Conta Corrente
+        </p>
+        {isBalanceVisible ? (
+          <p className="text-white text-[31px] font-normal">R$ 2.500,00</p>
+        ) : (
+          <p className="text-white text-[31px] font-normal">******</p>
+        )}
       </div>
     </div>
   );
