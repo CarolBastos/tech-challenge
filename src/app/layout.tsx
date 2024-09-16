9f163a4814dce38034c422d5af0ff23355be33c1
carolbastos <carol@wwcalc.com>
2024-09-16 09:33:57 -0400

    Ajustando RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.scss";
 export const metadata = {
   title: "Inicial",
   icons: {
-    icon: '/favicon_48px.svg',
+    icon: "/favicon_48px.svg",
   },
 };
 
@@ -13,17 +13,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <head>
-        <meta name="description" content="" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
-
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -3,6 +3,8 @@ import WelcomeLayout from '@/app/components/Layout/WelcomeLayout';
 
 export const metadata = {
   title: "Bytebank",
+  description:
+    "Descubra o Bytebank, onde você desfruta de uma conta e cartão gratuitos sem custos ocultos. Aproveite saques sem taxas e um programa de pontos que recompensa suas transações diárias. Proteja seus dispositivos com nosso seguro exclusivo e experimente a liberdade financeira que você merece. Escolha Bytebank para uma experiência bancária moderna e sem complicações.",
 };
 
 const WelcomePage: React.FC = () => (
