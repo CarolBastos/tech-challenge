97146fb13c71e4cee5804f275ca44e3c9178e68d
Tayná Ramos <tayna.ramos@ifood.com.br>
2024-10-12 15:29:23 -0300

    Criação de um arquivo para mock de statement

---
+++ src/app/api/statement/data.ts
@@ -0,0 +1,18 @@
+import { Statement } from "@/app/interfaces";
+
+export const statement: Statement = {
+  transactions: [
+    {
+      id: 1,
+      description: "depósito",
+      amount: 150,
+      date: "2022-11-18T14:30:45",
+    },
+    {
+      id: 2,
+      description: "transferência",
+      amount: 40,
+      date: "2022-11-13T14:30:45",
+    },
+  ],
+};
