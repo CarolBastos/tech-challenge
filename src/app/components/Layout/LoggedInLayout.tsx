b049ed30d5b3a27e58ded12190c1b6e98aea1c04
ig0rfranca <igorruizdefranca@gmail.com>
2024-10-27 19:08:52 -0300

    Componente de listagem das transações e componente do card para cada transação

--- src/app/components/Layout/LoggedInLayout.tsx
+++ src/app/components/Layout/LoggedInLayout.tsx
@@ -6,6 +6,8 @@ import NewTransaction from "../../components/new-transaction/new-transaction";
 
 import  "./layout.scss";
 import useAccount from '@/hooks/useAccount';
+import Image from 'next/image';
+import { TransactionCard } from '../generics/TransactionCard';
 
 const LoggedInLayout: React.FC = () => {
   const { user } = useAccount();
@@ -14,7 +16,7 @@ const LoggedInLayout: React.FC = () => {
     <div>
       <Header userName={user?.name} />
       <main className="main-logged bg-soft-green h-[100vh] w-full"> 
-        <section className="main-logged__section pt-6 flex justify-center items-center h-screen">
+        <section className="main-logged__section pt-6 flex justify-center items-start h-screen">
           <div className="main-logged__side-menu w-[180px] h-full bg-menu-gray rounded-lg">
             <Tabs />
           </div>
@@ -24,8 +26,34 @@ const LoggedInLayout: React.FC = () => {
             <NewTransaction />
           </div>
 
-          <div className="main-logged__bank-statement w-[282px] h-full bg-menu-gray">
-            <p>Extrato</p>
+          <div className="main-logged w-[282px] px-6 py-8 bg-menu-gray">
+            <div className='flex gap-12 items-center justify-between mb-6'>
+              <div className='font-bold text-xl'>
+                Extrato
+              </div>
+              <div className='flex justify-between gap-4'>
+                <button className='flex items-center justify-center rounded-full bg-sky-900 h-10 w-10'>
+                 <Image src="/images/pencil.svg" alt="imagem" width={24} height={36}/>
+                </button>
+                <button className='flex items-center justify-center rounded-full bg-sky-900 h-10 w-10'>
+                 <Image src="/images/trash.svg" alt="imagem" width={24} height={36}/>
+                </button>
+              </div>
+            </div>
+            <div className='flex flex-col gap-6'>
+              <TransactionCard
+                month='Novembro'
+                date='27/10/2024'
+                transactionType='Depósito'
+                transactionValue={30.00}
+              />
+              <TransactionCard
+                month='Novembro'
+                date='27/10/2024'
+                transactionType='Depósito'
+                transactionValue={30.00}
+              />
+            </div>
           </div>
         </section>
       </main>
