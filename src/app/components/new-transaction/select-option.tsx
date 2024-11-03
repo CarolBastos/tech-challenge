f2da48b16cfe14fc5b5641993c62c21c060dfce2
carolbastos <carol@wwcalc.com>
2024-11-02 23:33:37 -0400

    adicionando texto em negrito ao passar o mouse no item do select

--- src/app/components/new-transaction/select-option.tsx
+++ src/app/components/new-transaction/select-option.tsx
@@ -9,7 +9,7 @@ interface SelectOptionProps {
 const SelectOption: React.FC<SelectOptionProps> = ({ value, onClick, children }) => (
   <li className="select-option">
     <button
-      className="w-full text-sm text-neutral-400 p-6 hover:bg-tertiary-300"
+      className={`w-full text-sm text-neutral-400 p-6 hover:bg-tertiary-300 hover:font-bold`}
       onClick={() => onClick(value)}
     >
       {children}
