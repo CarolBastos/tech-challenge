7690ac7c319a446e1bb3ac1827a4e9c519721284
Tayná Ramos <tayna.ramos@ifood.com.br>
2024-11-05 12:20:29 -0300

    Criação de menu (drawer) para o header

--- src/app/components/header/welcome-header.tsx
+++ src/app/components/header/welcome-header.tsx
@@ -1,13 +1,27 @@
 import "./header.scss";
 
 import Image from "next/image";
+import Menu from "../menu/menu";
+import { useState } from "react";
 
 interface IWelcomeHeader {
-  onViewLoginForm: (value: boolean) => void
-  onViewUserRegisterForm: (value: boolean) => void
+  onViewLoginForm: (value: boolean) => void;
+  onViewUserRegisterForm: (value: boolean) => void;
 }
 
-export default function WelcomeHeader({onViewLoginForm, onViewUserRegisterForm}: IWelcomeHeader) {
+export default function WelcomeHeader({
+  onViewLoginForm,
+  onViewUserRegisterForm,
+}: IWelcomeHeader) {
+  const openMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="w-full bg-black text-sm text-white font-inter">
       <div className="max-w-sm flex flex-row-reverse items-center justify-between mx-auto py-6 md:flex-row md:max-w-md lg:max-w-lg lg:px-6">
@@ -31,31 +45,42 @@ export default function WelcomeHeader({onViewLoginForm, onViewUserRegisterForm}:
           <nav>
             <ul className="hidden md:flex items-center gap-4 inline text-md text-tertiary-500 font-semibold">
               <li>
-                <a href="#">Sobre</a>
+                <a href="/about">Sobre</a>
               </li>
               <li>
-                <a href="#">Serviços</a>
+                <a href="/services">Serviços</a>
               </li>
             </ul>
           </nav>
         </div>
-        <a href="/welcome" className="inline-block md:hidden lg:hidden">
+        <button
+          type="button"
+          onClick={openMenu}
+          className="inline-block md:hidden lg:hidden"
+        >
           <Image
             src="/images/icone-menu.svg"
-            alt="ByteBank logo"
+            alt="Menu"
             width={32}
             height={32}
           />
-        </a>
+        </button>
         <div className="hidden md:flex flex gap-4">
-          <button onClick={() => onViewUserRegisterForm(true)} className="bg-tertiary-500 text-white p-3.5 rounded-lg">
+          <button
+            onClick={() => onViewUserRegisterForm(true)}
+            className="bg-tertiary-500 text-white p-3.5 rounded-lg"
+          >
             Abrir conta
           </button>
-          <button onClick={() => onViewLoginForm(true)} className="border-2 border-tertiary-500 text-tertiary-500 font-semibold p-3.5 rounded-lg">
+          <button
+            onClick={() => onViewLoginForm(true)}
+            className="border-2 border-tertiary-500 text-tertiary-500 font-semibold p-3.5 rounded-lg"
+          >
             Já tenho conta
           </button>
         </div>
       </div>
+      <Menu isOpen={isMenuOpen} onClose={closeMenu} />
     </header>
   );
 }
